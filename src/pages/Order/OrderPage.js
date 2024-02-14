03de243c639f6e55bc06a15e98955e71bb1b0590
Precious Eruteya <eruteyaprecious@gmail.com>
2024-02-14 13:55:17 +0100

    CodeBook Deployment

---
+++ src/pages/Order/OrderPage.js
@@ -0,0 +1,16 @@
+import { useLocation } from "react-router-dom";
+import { OrderFail } from "./components/OrderFail";
+import { OrderSuccess } from "./components/OrderSuccess";
+import { useTitle } from "../../hooks/useTitle";
+
+export const OrderPage = () => {
+    useTitle("Order Summary");
+    const { state } = useLocation();
+    // const status = true;
+  return (
+    <main>
+      { state.status ? <OrderSuccess data={state.data} /> : <OrderFail /> }
+    </main>
+  )
+}
+
